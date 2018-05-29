6de49f4280105515716a7e5773245bfd73335978
Ryan Robertson <rjr.devry@gmail.com>
2018-05-29 00:08:46 -0700

    Finish Chapter 4.  Add mysql db and use template_file for user_data scripts.

---
+++ stage/data-stores/mysql/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-up-and-running-rjr"
+    key            = "stage/data-stores/mysql/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "example" {
+  engine            = "mysql"
+  allocated_storage = 10
+  instance_class    = "db.t2.micro"
+  name              = "example_database"
+  username          = "admin"
+  password          = "${var.db_password}"
+}
--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -17,11 +17,7 @@ resource "aws_launch_configuration" "example" {
   instance_type   = "t2.micro"
   security_groups = ["${aws_security_group.instance.id}"]
 
-  user_data = <<-EOF
-    #!/bin/bash
-    echo "Hello, World" > index.html
-    nohup busybox httpd -f -p "${var.server_port}" &
-    EOF
+  user_data = "${data.template_file.user_data.rendered}"
 
   lifecycle {
     create_before_destroy = true
@@ -102,3 +98,23 @@ resource "aws_security_group" "elb" {
 }
 
 data "aws_availability_zones" "all" {}
+
+data "terraform_remote_state" "db" {
+  backend = "s3"
+
+  config {
+    bucket = "terraform-up-and-running-rjr"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "template_file" "user_data" {
+  template = "${file("user-data.sh")}"
+
+  vars {
+    server_port = "${var.server_port}"
+    db_address  = "${data.terraform_remote_state.db.address}"
+    db_port     = "${data.terraform_remote_state.db.port}"
+  }
+}
