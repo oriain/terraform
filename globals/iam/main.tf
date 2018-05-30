e6b856f6453471011c2b0b2dc15e385d45e50041
Ryan Robertson <rjr.devry@gmail.com>
2018-05-29 21:07:50 -0700

    Add count-conditonal IAM user policies for CloudWatch for neo.

--- globals/iam/main.tf
+++ globals/iam/main.tf
@@ -35,3 +35,43 @@ resource "aws_iam_policy_attachment" "ec2_access" {
   users      = "${var.user_names}"
   policy_arn = "${aws_iam_policy.ec2_read_only.arn}"
 }
+
+data "aws_iam_policy_document" "cloudwatch_read_only" {
+  statement {
+    effect    = "Allow"
+    actions   = ["cloudwatch:Describe*", "cloudwatch:Get*", "cloudwatch:List*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "cloudwatch_read_only" {
+  name   = "cloudwatch-read-only"
+  policy = "${data.aws_iam_policy_document.cloudwatch_read_only.json}"
+}
+
+data "aws_iam_policy_document" "cloudwatch_full_access" {
+  statement {
+    effect    = "Allow"
+    actions   = ["cloudwatch:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "cloudwatch_full_access" {
+  name   = "cloudwatch-full-access"
+  policy = "${data.aws_iam_policy_document.cloudwatch_full_access.json}"
+}
+
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_full_access" {
+  count = "${var.give_neo_cloudwatch_full_access}"
+
+  user       = "${aws_iam_user.example.0.arn}"
+  policy_arn = "${aws_iam_policy.cloudwatch_full_access.arn}"
+}
+
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_read_only" {
+  count = "${1 - var.give_neo_cloudwatch_full_access}"
+
+  user       = "${aws_iam_user.example.0.arn}"
+  policy_arn = "${aws_iam_policy.cloudwatch_read_only.arn}"
+}
--- globals/iam/vars.tf
+++ globals/iam/vars.tf
@@ -3,3 +3,7 @@ variable "user_names" {
   type        = "list"
   default     = ["neo", "trinity", "morpheus"]
 }
+
+variable "give_neo_cloudwatch_full_access" {
+  description = "If true, neo gets full access to CloudWatch."
+}
