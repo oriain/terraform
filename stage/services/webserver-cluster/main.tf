13120b6f27c2a32424c0dfad1e591d37940ffa7b
Ryan Robertson <rjr.devry@gmail.com>
2018-05-30 18:22:13 -0700

    Set AMI and server_text module variables.

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -15,15 +15,17 @@ provider "aws" {
 module "webserver_cluster" {
   source = "git::git@github.com:oriain/terraform-modules.git//services/webserver-cluster?ref=v0.0.1"
 
+  ami         = "ami-40d28157"
+  server_text = "New server text"
+
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "terraform-up-and-running-rjr"
   db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
 
-  instance_type        = "t2.micro"
-  min_size             = 2
-  max_size             = 2
-  enable_autoscaling   = false
-  enable_new_user_data = true
+  instance_type      = "t2.micro"
+  min_size           = 2
+  max_size           = 2
+  enable_autoscaling = false
 }
 
 resource "aws_security_group_rule" "allow_testing_inbound" {
